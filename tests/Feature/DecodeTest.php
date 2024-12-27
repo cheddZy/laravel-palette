f8e5d6ebdcdf49c47077121c2ae1cccffa866dcd
cheddZy <cheddZy@users.noreply.github.com>
2024-12-27 09:08:22 +0000

    Fix styling

--- tests/Feature/DecodeTest.php
+++ tests/Feature/DecodeTest.php
@@ -5,26 +5,26 @@ use Cheddzy\LaravelPalette\ColorPalette;
 it('can decode palettes', function () {
     $expectedPalette = [
         [
-            "title" => "Test Palette",
-            "colors" => [
-                0 => ["red" => 255, "green" => 0, "blue" => 0],
-                1 => ["red" => 0, "green" => 255, "blue" => 0],
-                2 => ["red" => 0, "green" => 0, "blue" => 255],
-            ]
+            'title' => 'Test Palette',
+            'colors' => [
+                0 => ['red' => 255, 'green' => 0, 'blue' => 0],
+                1 => ['red' => 0, 'green' => 255, 'blue' => 0],
+                2 => ['red' => 0, 'green' => 0, 'blue' => 255],
+            ],
         ],
         [
-            "title" => "Test Palette",
-            "colors" => [
-                0 => ["red" => 255, "green" => 0, "blue" => 0],
-                1 => ["red" => 0, "green" => 255, "blue" => 0],
-                2 => ["red" => 0, "green" => 0, "blue" => 255],
-            ]
+            'title' => 'Test Palette',
+            'colors' => [
+                0 => ['red' => 255, 'green' => 0, 'blue' => 0],
+                1 => ['red' => 0, 'green' => 255, 'blue' => 0],
+                2 => ['red' => 0, 'green' => 0, 'blue' => 255],
+            ],
         ],
     ];
 
-    $decoder = new ColorPalette();
+    $decoder = new ColorPalette;
 
-    $output = $decoder->decode(__DIR__ . '/assets/test.cpal');
+    $output = $decoder->decode(__DIR__.'/assets/test.cpal');
 
     expect($output)->toEqual($expectedPalette);
 });
